8f56a2fe7057ea1173bbc24433b157dce01bd21c
Christian <chris@rubyonrails.com>
2017-09-27 19:20:13 -0700

    Fix create, add, and flash

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,12 +5,20 @@ class ArticlesController < ApplicationController
 
 	def create 
 		# Going to display the information 
-		#render plain: params[:article].inspect 
-
+		
 		@article = Article.new(article_params)
-		#Saving the article:
-		@article.save
-		redirect_to articles_show(@article)
+		#Saving the article:		
+		if @article.save
+			# Going to display the information 
+			flash[:notice] = "Article was successfully created!"
+			redirect_to article_path(@article)
+		else 
+			render 'new'
+		end 
+	end 
+
+	def show 
+		@article = Article.find(params[:id])
 	end 
 
 	private 
